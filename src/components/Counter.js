94845fac40c90b5e3fae99b58455eace8b8ec95d
americanman <snowboardracer7@gmail.com>
2018-12-02 18:08:02 -0500

    Render first component

---
+++ src/components/Counter.js
@@ -0,0 +1,17 @@
+import React,  { Component } from 'react';
+
+class Counter extends Component {
+
+  render() {
+
+    return(
+      <div>
+        <button>Increment</button>
+        <button>Decrement</button>
+      </div>
+
+    )
+  }
+}
+
+export default Counter
